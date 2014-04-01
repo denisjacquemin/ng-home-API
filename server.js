f2d5075ac3f7ea133ed2bcf1b687763c5875efd2
Denis Jacquemin <denis.jacquemin@gmail.com>
2014-04-01 08:06:10 +0200

    adding route with geospatial query on 2dsphere index

--- server.js
+++ server.js
@@ -28,7 +28,32 @@ var Property = mongoose.model('Property', propertySchema, 'properties');
 
 var app = express().use(cors());
 
+app.get('/API/v1/long', function (req, res) {
+
+    var lon = 40;
+    var lat = 5;
+
+    Property.find({
+        loc: {
+            $near : {
+                $geometry : {
+                    type: "Point",
+                    coordinates: [lon, lat]
+                }
+            },
+            $maxdistance: 500
+        }
+    }, function (err, doc) {
+        res.send(doc);
+    });
+});
+
 app.get('/API/v1/:city', function (req, res) {
+
+    // get the long and loc of city
+    // make a geoSearch with the map box
+    // and return the collection
+
     var addressFilter = new RegExp(req.params.city, "i");
 
     Property.find({ address: addressFilter },
@@ -36,5 +61,9 @@ app.get('/API/v1/:city', function (req, res) {
             res.send(doc);
         });
 });
+
+
+
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
